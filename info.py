944bf674d68a0808133c5ad58dd609c76c4c35b1
Ganeshkarale212002 <98474751+Ganeshkarale212002@users.noreply.github.com>
2023-08-26 22:34:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6682653076:AAGvvdmboQ2Js9HLeF_UUlCFwaexUwc
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/874e7fe7a85731083c800.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/0b4f09f8f2704885781e5.jpg')).split()
 
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1089524901').split()]
